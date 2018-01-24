5fbb9926a010178b9fb4f153bf78ed0cba68d03b
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-01-23 23:31:51 -0300

    Create a basic layout to find songs

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,50 @@
 <template lang="pug">
   #app
-    img(src="./assets/logo.png")
-    h1 {{ msg }}
+    section.section
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(
+            type="text"
+            placeholder="Search songs"
+            v-model="searchQuery"
+          )
+          a.button.is-info.is-large(@click="search") Search
+          a.button.is-danger.is-large &times;
+
+          p
+            small {{results}}
+
+      .container
+        .columns
+          .column(v-for="track in tracks")
+            p {{track.name}} - {{track.artist}}
 </template>
 
 <script>
+
+const tracks = [
+  { name: 'Song 1', artist: 'Artist 1' },
+  { name: 'Song 2', artist: 'Artist 2' },
+  { name: 'Song 3', artist: 'Artist 3' },
+  { name: 'Song 4', artist: 'Artist 4' }
+]
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      searchQuery: '',
+      tracks: []
+    }
+  },
+  methods: {
+    search () {
+      this.tracks = tracks
+    }
+  },
+  computed: {
+    results () {
+      return `${this.tracks.length} results`
     }
   }
 }
