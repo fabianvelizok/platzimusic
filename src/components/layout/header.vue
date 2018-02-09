9d31408f63afb57b4c8172db528491cd6a0266d1
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-02-08 23:57:46 -0300

    Update header

--- src/components/layout/header.vue
+++ src/components/layout/header.vue
@@ -1,3 +1,15 @@
 <template lang="pug">
-  h1 Header
+  section.hero.is-success
+    .hero-head
+      header.nav
+        .container
+          .nav-left
+            .nav-item
+              strong 🎧 Platzi Music
+          .nav-right.nav-menu
+
+    .hero-body
+      .container.has-text-centered
+        h1.title Platzi Music
+        h2.subtitle Great tracks!
 </template>
